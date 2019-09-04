d2ee3b58bbcc58ec47bf35f0361d89e8e2a35b61
Cedric Kim <hello@cedric.today>
2019-09-04 19:57:16 +0200

    Fix Waketag
    I fixed Waketag, so now it builds!

---
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -0,0 +1,44 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-plugin-device.device",
+      "file": "plugins/cordova-plugin-device/www/device.js",
+      "pluginId": "cordova-plugin-device",
+      "clobbers": [
+        "device"
+      ]
+    },
+    {
+      "id": "cordova-plugin-background-mode.BackgroundMode",
+      "file": "plugins/cordova-plugin-background-mode/www/background-mode.js",
+      "pluginId": "cordova-plugin-background-mode",
+      "clobbers": [
+        "cordova.plugins.backgroundMode",
+        "plugin.backgroundMode"
+      ]
+    },
+    {
+      "id": "cordova-plugin-drawoverapps.DrawOverApps",
+      "file": "plugins/cordova-plugin-drawoverapps/www/OverApps.js",
+      "pluginId": "cordova-plugin-drawoverapps",
+      "clobbers": [
+        "window.overApps"
+      ]
+    },
+    {
+      "id": "phonegap-nfc.NFC",
+      "file": "plugins/phonegap-nfc/www/phonegap-nfc.js",
+      "pluginId": "phonegap-nfc",
+      "runs": true
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-androidx": "1.0.2",
+    "cordova-plugin-androidx-adapter": "1.1.0",
+    "cordova-plugin-device": "2.0.3",
+    "cordova-plugin-background-mode": "0.7.3",
+    "cordova-plugin-drawoverapps": "1.0.6",
+    "cordova-plugin-whitelist": "1.3.4",
+    "phonegap-nfc": "1.0.3"
+  };
+});
