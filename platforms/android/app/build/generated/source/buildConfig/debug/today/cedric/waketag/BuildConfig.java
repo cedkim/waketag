0709dda15735f944af369a355914017de3df0865
Cedric Kim <hello@cedric.today>
2019-09-04 15:17:25 +0200

    Build attempt
    I attempted a build (and failed).

---
+++ platforms/android/app/build/generated/source/buildConfig/debug/today/cedric/waketag/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package today.cedric.waketag;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "today.cedric.waketag";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 100;
+  public static final String VERSION_NAME = "0.1.0";
+}
