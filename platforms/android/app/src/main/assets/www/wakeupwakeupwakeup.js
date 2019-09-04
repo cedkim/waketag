f05929e55bc3ee04df13dd202b489f71818f2336
Cedric Kim <hello@cedric.today>
2019-09-04 15:09:34 +0200

    Install everything
    I installed everything I needed via cordova.

---
+++ platforms/android/app/src/main/assets/www/wakeupwakeupwakeup.js
@@ -0,0 +1,16 @@
+/* Waketag */
+var tapped;
+tapped = false;
+document.addEventListener("deviceready", function() {
+  beepBeepBeep = new Media("beep.wav", function(){
+    beepBeepBeep.seekTo(0);
+    beepBeepBeep.play();
+  });
+  beepBeepBeep.play();
+  nfc.addNdefListener(function() {
+    if (tapped != true) {
+      tapped = false;
+      window.overApps.closeOverApp();
+    }
+  });
+}, false);
