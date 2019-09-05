64393023168ab66484dbe0c4d4ec33747293a1f9
Cedric Kim <hello@cedric.today>
2019-09-05 11:06:05 +0200

    Use NativeStorage
    I replaced localStorage with NativeStorage.

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -30,6 +30,24 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "file": "plugins/phonegap-nfc/www/phonegap-nfc.js",
       "pluginId": "phonegap-nfc",
       "runs": true
+    },
+    {
+      "id": "cordova-plugin-nativestorage.mainHandle",
+      "file": "plugins/cordova-plugin-nativestorage/www/mainHandle.js",
+      "pluginId": "cordova-plugin-nativestorage",
+      "clobbers": [
+        "NativeStorage"
+      ]
+    },
+    {
+      "id": "cordova-plugin-nativestorage.LocalStorageHandle",
+      "file": "plugins/cordova-plugin-nativestorage/www/LocalStorageHandle.js",
+      "pluginId": "cordova-plugin-nativestorage"
+    },
+    {
+      "id": "cordova-plugin-nativestorage.NativeStorageError",
+      "file": "plugins/cordova-plugin-nativestorage/www/NativeStorageError.js",
+      "pluginId": "cordova-plugin-nativestorage"
     }
   ];
   module.exports.metadata = {
@@ -39,6 +57,7 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
     "cordova-plugin-background-mode": "0.7.3",
     "cordova-plugin-drawoverapps": "1.0.6",
     "cordova-plugin-whitelist": "1.3.4",
-    "phonegap-nfc": "1.0.3"
+    "phonegap-nfc": "1.0.3",
+    "cordova-plugin-nativestorage": "2.3.2"
   };
 });
