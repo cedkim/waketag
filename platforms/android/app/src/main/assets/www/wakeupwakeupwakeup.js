63a97b7735f5b1c0507542e26157c645d48ec9d2
Cedric Kim <hello@cedric.today>
2019-09-06 08:46:27 +0200

    Run build
    I ran a build on the whole project.

--- platforms/android/app/src/main/assets/www/wakeupwakeupwakeup.js
+++ platforms/android/app/src/main/assets/www/wakeupwakeupwakeup.js
@@ -1,16 +1,18 @@
 /* Waketag */
 var tapped;
 tapped = false;
-document.addEventListener("deviceready", function() {
-  beepBeepBeep = new Media("beep.wav", function(){
-    beepBeepBeep.seekTo(0);
+document.addEventListener("DOMContentLoaded", function() {
+  document.addEventListener("deviceready", function() {
+    beepBeepBeep = new Media("beep.wav", function(){
+      beepBeepBeep.seekTo(0);
+      beepBeepBeep.play();
+    });
     beepBeepBeep.play();
-  });
-  beepBeepBeep.play();
-  nfc.addNdefListener(function() {
-    if (tapped != true) {
-      tapped = false;
-      window.overApps.closeOverApp();
-    }
-  });
-}, false);
+    nfc.addNdefListener(function() {
+      if (tapped != true) {
+        tapped = false;
+        window.overApps.closeOverApp();
+      }
+    });
+  }, false);
+});
